02617b1b80a0e8409f81261c3d8cf5c4eb40c86c
gabrielferrazz <gabrielrferraz@hotmail.com>
2023-10-12 00:47:59 -0300

    dashboard 365

---
+++ licence365/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='flex-1 items-center justify-center'>
+      <header className='flex items-center justify-center'>
+        <h1>Dashboard</h1>
+        <h1>.365</h1>
+      </header>
+    </div>
+  )
+}
